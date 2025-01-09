f6383401b9cf54400928a7474fd125f1a82dd1eb
hwakabh <hrykwkbys1024@gmail.com>
2025-01-10 01:20:21 +0900

    feat: implemented valildate title functions with @conventional-commits/parser.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { parser, toConventionalChangelogFormat } = require('@conventional-commits/parser');
 
-console.log("Starting actions: @hwakabh/semantic-issue-action")
+console.log("Starting actions: @hwakabh/semantic-issue-action");
 
 try {
   // Fetch input values from action-metadata using `use.with` statement
   const targetRepo = core.getInput('repo');
-  // TODO: validate string of repo (owner/reponame)
-
   const ghToken = core.getInput('token');
-  const octokit = github.getOctokit(ghToken);
 
+  // TODO: validate string of repo (owner/reponame)
+  // TODO: validate existence of repo in github.com
+
+  const octokit = github.getOctokit(ghToken);
   console.log(`Fetching issues in repository ${targetRepo}`);
+
+  // TODO: Fetch target issue object from action contexts (workflow triggers)
   octokit.rest.issues.listForRepo({
     owner: "hwakabh",
     repo: "semantic-issue-action"
@@ -20,14 +24,17 @@ try {
     // console.log(issues.data);
     issues.data.forEach(i => {
       console.log(i.title);
+      if (isSemantic(i.title)) {
+        console.log('>>> Issue title is semantic');
+        core.setOutput("check-result", true);
+      } else {
+        console.log('>>> Not semantic!');
+        core.setOutput("check-result", false);
+
+        // TODO: post comments to the issue
+      }
     });
   })
-
-
-  // define returns of actions
-  const result = "https://github.com/" + targetRepo;
-  console.log("Set output values as check-result");
-  core.setOutput("check-result", result);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -35,4 +42,16 @@ try {
 
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+function isSemantic(issueTitle) {
+  try {
+    const ast = parser(issueTitle);
+    const commits = toConventionalChangelogFormat(ast);
+    console.log(commits);
+    return true
+  } catch {
+    return false
+  }
 }
