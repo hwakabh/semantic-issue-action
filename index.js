2c82fa97698f9913904c1dbd048fe10e2a3415b7
hwakabh <hrykwkbys1024@gmail.com>
2025-01-09 13:27:05 +0900

    feat: added initial core logics.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world from JavaScript gh-actions");
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+console.log("Starting actions: @hwakabh/semantic-issue-action")
+
+try {
+  // Fetch input values from action-metadata using `use.with` statement
+  const targetRepo = core.getInput('repo');
+  console.log(`Checking issue title(s) in repository ${targetRepo}`);
+
+  // TODO: validate string of repo (owner/reponame)
+
+  // define returns of actions
+  const result = "https://github.com/" + targetRepo;
+  console.log("Set output values as check-result");
+  core.setOutput("check-result", result);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
